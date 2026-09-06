e3504f42bf1aa82819abdf3f51ba93ef5791aa7d
Koon-Chun <dimitle.koonchun@proton.me>
2026-09-06 08:01:36 +0200

    fade effect p2

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,11 @@ let fadeElements = document.querySelectorAll('.fade-in');
 
 function fadeInElements() {
     for (let i = 0; i < fadeElements.length; i++) {
-        let timer = (i + 1) * 500; // Delay each element by 500ms;
+        let timer = Math.random()*2.85;
+        timer = timer.toFixed(2)+'s';
+        
+        let delay = Math.random()*2.85;
+        delay = delay.toFixed(2)+'s';;
 
         fadeElements[i].style.transition = 'opacity ' + timer + 'ms';
         fadeElements[i].style.opacity = 1;
